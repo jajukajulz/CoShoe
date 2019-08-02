a35e6c8005d29a65fbb363a99095c832efcd7e8b
juliank <julian.kanjere@jskdezign.com>
2019-08-02 21:37:33 +0200

    Import OpenZeppelin ERC721, update constructor and minting

--- contracts/CoShoe.sol
+++ contracts/CoShoe.sol
@@ -1,27 +1,55 @@
 pragma solidity ^0.5.0; //solidity version
 
-//Contract 
-contract CoShoe is ERC721 {
+import "../node_modules/zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
-    // The keyword public makes the variable readable from outside of the contract.
+/**
+ * @dev Implementation of ERC-721 non-fungible token standard.
+ */
+contract CoShoe is ERC721Token {
+
+    /**
+    *  @dev Ethereum address that deployed the contract. The keyword public makes the variable readable from outside of the contract.
+    */
     address public minter;
 
-    /*a shoe struct holding the shoe’s owner (address), name (string),
-    an image (url where it can be downloaded), sold  (boolean) */
+    /**
+    *  @dev Shoe struct holding the shoe’s owner (address), name (string),
+    * an image (url where it can be downloaded), sold  (boolean)
+    */
     struct Shoe {
         address payable owner;
         string name;
         string image; //url to the image
         bool sold;
     }
-    //state variables
-    uint price = 0.5/(1 ether); //ether converted to wei
-    uint shoesSold = 0; //number of shoes sold
-    uint numTokensToMint = 100; //number of tokens to mint on deployment of contract
 
-    Shoe[] public shoes;  //array of shoes made public because I have length method
+    /**
+    *  @dev state variables
+    */
+    uint8 price = 0.5/(1 ether); //ether converted to wei
+    uint8 numTokensToMint = 100; //number of tokens to mint on deployment of contract
+    uint256 shoesSold = 0; //number of shoes sold
+    string _tokenURI = "CoShoe Token";
+    /**
+    *  @dev Array of shoes. Made public because of length method.
+    */
+    Shoe[] public shoes;
 
-    constructor () public {
+    /**
+    * @dev An event for lightweight clients when a new shoe is created.
+    * @param _shoeId The NFT that got created.
+    */
+    event registeredShoeEvent (
+        uint indexed _shoeId
+    );
+
+    /**
+    * @dev Contract constructor.
+    */
+    constructor (string _name, string _symbol) public {
+        //required in OpenZeppelin implementation
+        ERC721Token(_name, _symbol);
+
         //set minter to Ethereum address that deployed the contract
         minter = msg.sender;
 
@@ -32,13 +60,6 @@ contract CoShoe is ERC721 {
     }
 
     /**
-    * @dev An event for lightweight clients when a new shoe is created.
-    */
-    event registeredShoeEvent (
-        uint indexed _shoeId
-    );
-
-    /**
     * @dev A function to create a new shoe pair (i.e. a token).
     * The owner of each token is the address deploying the contract (msg.sender),
     * name and image are empty strings( “” ), and sold is equal to false.
@@ -47,13 +68,14 @@ contract CoShoe is ERC721 {
     */
     function createShoePair(address _to) public {
         //create new pair of shoes, push to array and return position
-        uint id = (shoes.push(Shoe(_to, "", "", false)))-1;
+        uint256 _tokenId = (shoes.push(Shoe(_to, "", "", false)))-1;
 
         // trigger registeredShoe event
-        emit registeredShoeEvent(id);
+        emit registeredShoeEvent(_tokenId);
 
-        // Assign token to the Ethereum Address that deployed the contract i.e minter
-        _mint(_to, id);
+        // Create unique token by calling Open-Zeppelin function
+        super._mint(_to, _tokenId);
+        super._setTokenURI(_tokenId, _tokenURI);
     }
 
     /**
